c8be35ecfd43ee5740038f877c29af69cae8ceb7
zabed <zabedalam777@gmail.com>
2021-01-01 23:40:56 +0100

    HTML template to React Template done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './app/App.jsx';
 import reportWebVitals from './reportWebVitals';
+import './assets/styles/reset.css'
+import './assets/styles/style.css'
 
 ReactDOM.render(
   <React.StrictMode>
