07090623b4ab197daab41450c0077e8d1ee9fea2
zabed <zabedalam777@gmail.com>
2021-01-11 12:54:14 +0100

    nav component created

--- src/pages/buckets/index.jsx
+++ src/pages/buckets/index.jsx
@@ -1,6 +1,5 @@
 // Import Assets
-import brandLogo from '../../assets/brand-logo/brand-logo.svg';
-import moreMenu from '../../assets/icons/more-menu.png';
+import Nav from '../../components/nav'
 import plusIcon from '../../assets/icons/plus-icon.png';
 import editIcon from '../../assets/icons/edit-icon.png';
 import deleteIcon from '../../assets/icons/delete-icon.png';
@@ -10,27 +9,7 @@ function BucketPage() {
 	return (
 		<div>
 			{/* Navigation Bar */}
-			<nav className='nav __shadow--lg'>
-				<div className='container nav__container'>
-					<div className='nav__brand'>
-						<img
-							className='nav__brand-logo'
-							src={brandLogo}
-							alt='Brand Logo'
-						/>
-						<h1 className='nav__brand-name'>Shopping List</h1>
-					</div>
-					<div className='nav__menu nav__menu--right'>
-						<div className='nav__menu-items'>
-							<img
-								className='nav__menu-icon'
-								src={moreMenu}
-								alt='More Menu'
-							/>
-						</div>
-					</div>
-				</div>
-			</nav>
+			<Nav/>
 			{/* Main Content Body */}
 			<main className='container __margin--ylg'>
 				{/* Add New Bucket Section */}
